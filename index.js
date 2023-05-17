25c5d778694e4600cd98a5a59a910b1246d4a179
ErickAvellano <erickdevega.avellano@bicol-u.edu.ph>
2023-05-17 08:13:27 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
+
+  // Server-side code
+io.on('connection', function(socket) {
+  socket.on('disconnect', function() {
+    io.emit('user_left', socket.username); // Notify all clients about the user leaving the chat
+  });
+
+  socket.on('username', function(username) {
+    socket.username = username;
+    io.emit('is_online', socket.username); // Notify all clients about the user joining the chat
+  });
+});
+
+server = http.listen(8000, function() {
+  console.log('listening on *:8000');
+});
